40b899d19e50c372879da9fab37e37d8f78c2457
Michael <michael_kirsch@t-online.de>
2021-03-21 22:22:30 +0100

    glad implementation

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
-
-
 #include "shitty_support_header/shitty_support.h"
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <chrono>
+#include <thread>
+
+
 
 const GLint WIDTH = 800, HEIGHT = 700;
 
 using namespace ShittySupportLib;
+
+GLuint VAO,VBO,shader;
+
+const int Framerate = 60;
+
+
+void createTrangle()
+{
+    GLfloat vertices[]={
+            -1.f,-1.f,0.f,
+            1.f,-1.f,0.f,
+            0.f,1.f,0.f
+    };
+
+    glad_glGenVertexArrays(1,&VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+
+
+};
+
+
 
 int main() {
     Debugger::instance().log("GLFW init",glfwInit());
@@ -35,13 +61,23 @@ int main() {
     Debugger::instance().print_log();
     Debugger::instance().clear_log();
 
+    SimpleClock clock1;
+
+
+
+
     while (!glfwWindowShouldClose(mainWindow))
     {
-        glfwPollEvents();
-        glClearColor(1.f,0.2,0.5,0.1);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(mainWindow);
+            glfwPollEvents();
+            glClearColor(1.f,0.2,0.5,0.1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(mainWindow);
+            std::this_thread::sleep_for(std::chrono::milliseconds(int(1000/Framerate)));
     }
+
+
+
+
 
 
 
